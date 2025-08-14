a181c8abc0df7255aab27a65273cb04bf9f53c16
Y0del <149356173+Y0del@users.noreply.github.com>
2025-08-14 14:52:31 +0900

    프로젝트 파일 업로드
    현재 진행도 합침

---
+++ Source/Exhibition/ExhibitionGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ExhibitionGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AExhibitionGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AExhibitionGameMode();
+};
+
+
+
---
+++ Source/Exhibition/ExhibitionPlayerController.cpp
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "ExhibitionPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+
+void AExhibitionPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// get the enhanced input subsystem
+	if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+	{
+		// add the mapping context so we get controls
+		Subsystem->AddMappingContext(InputMappingContext, 0);
+
+		UE_LOG(LogTemp, Warning, TEXT("BeginPlay"));
+	}
+}
